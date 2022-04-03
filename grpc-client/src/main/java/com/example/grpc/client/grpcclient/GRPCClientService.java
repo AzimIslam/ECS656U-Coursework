0797e1964ce692466a3cf5bcd6f86b5d98fc1646
Azim Islam <ec19205@qmul.ac.uk>
2022-04-03 16:33:21 +0100

    Add scaling for multiplication

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -4,6 +4,7 @@ import com.example.grpc.server.grpcserver.PingRequest;
 import com.example.grpc.server.grpcserver.PongResponse;
 import com.example.grpc.server.grpcserver.PingPongServiceGrpc;
 import com.example.grpc.server.grpcserver.MatrixRequest;
+import com.example.grpc.server.grpcserver.MatrixMultParallelRequest;
 import com.example.grpc.server.grpcserver.MatrixReply;
 import com.example.grpc.server.grpcserver.MatrixServiceGrpc;
 import com.example.grpc.server.grpcserver.Row;
@@ -62,9 +63,6 @@ public class GRPCClientService {
 		if (m1.length > 2) {
 			int numOfServersRequired = m1.length == 4 ? 4: 8;
 			int numberOfRows = m1.length / numOfServersRequired ;
-
-			System.out.println("Number of rows per server: " + numberOfRows);
-			System.out.println("Number of servers required: " + numOfServersRequired);
 
 			MatrixRequest.Builder[] requests = new MatrixRequest.Builder[numOfServersRequired];
 
@@ -139,7 +137,6 @@ public class GRPCClientService {
 			List<Row> arrayListC = C.getCList();
 
 			// Iterates over result matrix
-
 			for (int row = 0; row < arrayListC.size(); row++) {
 				for (int col = 0; col < arrayListC.get(row).getNumberList().size(); col++) {
 					resp += arrayListC.get(row).getNumber(col) + " ";
@@ -153,46 +150,119 @@ public class GRPCClientService {
     }
 
 	public String multiply(double[][] m1, double[][] m2) {
-		ManagedChannel channel = ManagedChannelBuilder.forAddress("10.128.0.12",9090)
-		.usePlaintext()
-		.build();
-		MatrixServiceGrpc.MatrixServiceBlockingStub stub
-		 = MatrixServiceGrpc.newBlockingStub(channel);
+		final String[] IP_ADDR = {"35.222.9.50", "34.66.0.91", "35.238.180.188",  "34.69.186.132", "34.67.18.167", "35.194.11.26", "34.67.205.167", "34.133.18.14"};
+		
+		final ManagedChannel[] channels = {
+			ManagedChannelBuilder.forAddress(IP_ADDR[0], 9090).usePlaintext().build(), 
+			ManagedChannelBuilder.forAddress(IP_ADDR[1], 9090).usePlaintext().build(),
+			ManagedChannelBuilder.forAddress(IP_ADDR[2], 9090).usePlaintext().build(),
+			ManagedChannelBuilder.forAddress(IP_ADDR[3], 9090).usePlaintext().build(),
+			ManagedChannelBuilder.forAddress(IP_ADDR[4], 9090).usePlaintext().build(),
+			ManagedChannelBuilder.forAddress(IP_ADDR[5], 9090).usePlaintext().build(),
+			ManagedChannelBuilder.forAddress(IP_ADDR[6], 9090).usePlaintext().build(),
+			ManagedChannelBuilder.forAddress(IP_ADDR[7], 9090).usePlaintext().build()
+		};
 
-		MatrixRequest.Builder request = MatrixRequest.newBuilder();
+		final MatrixServiceGrpc.MatrixServiceBlockingStub[] stubs = {
+			MatrixServiceGrpc.newBlockingStub(channels[0]),
+			MatrixServiceGrpc.newBlockingStub(channels[1]),
+			MatrixServiceGrpc.newBlockingStub(channels[2]),
+			MatrixServiceGrpc.newBlockingStub(channels[3]),
+			MatrixServiceGrpc.newBlockingStub(channels[4]),
+			MatrixServiceGrpc.newBlockingStub(channels[5]),
+			MatrixServiceGrpc.newBlockingStub(channels[6]),
+			MatrixServiceGrpc.newBlockingStub(channels[7]),
+		};
 
-		for (int row = 0; row < m1.length; row++) {
-			Row.Builder tempRow = Row.newBuilder();
-			for (int col = 0; col < m1[row].length; col++) {
-				tempRow.addNumber(m1[row][col]);
-			}
-			request.addA(tempRow);
-		}
-
-		for (int row = 0; row < m2.length; row++) {
-			Row.Builder tempRow = Row.newBuilder();
-			for (int col = 0; col < m2[row].length; col++) {
-				tempRow.addNumber(m2[row][col]);
-			}
-			request.addB(tempRow);
-		}
-
-
-
-
-		MatrixReply C=stub.multiplyBlock(request.build());
-		List<Row> arrayListC = C.getCList();
-
-		// Iterates over result matrix
 		String resp="";
 
-		for (int row = 0; row < arrayListC.size(); row++) {
-			for (int col = 0; col < arrayListC.get(row).getNumberList().size(); col++) {
-				resp += arrayListC.get(row).getNumber(col) + " ";
+		if (m1.length > 2) {
+			int numOfServersRequired = m1.length == 4 ? 4: 8;
+			int numberOfRows = m1.length / numOfServersRequired;
+
+			MatrixMultParallelRequest.Builder[] requests = new MatrixMultParallelRequest.Builder[numOfServersRequired];
+
+			for (int i = 0; i < requests.length; i++) {
+				requests[i] = MatrixMultParallelRequest.newBuilder();
 			}
-			resp += "<br>";
+
+			int serverPtr = 0;
+			int row = 0;
+
+			while(serverPtr < numOfServersRequired) {
+				requests[serverPtr].setStart(row);
+				requests[serverPtr].setEnd(row+numberOfRows);
+
+				for (int i = row; i < row + numberOfRows; i++) {
+					Row.Builder tempRow = Row.newBuilder();
+					for (int col = 0; col < m1.length; col++) {
+						tempRow.addNumber(m1[i][col]);
+					}
+					requests[serverPtr].addA(tempRow);
+				}
+
+				for (int i = row; i < row + numberOfRows; i++) {
+					Row.Builder tempRow2 = Row.newBuilder();
+					for (int col = 0; col < m2.length; col++) {
+						tempRow2.addNumber(m2[i][col]);
+					}
+					requests[serverPtr].addB(tempRow2);
+				}
+
+				row += numberOfRows;
+				serverPtr += 1;
+			}
+
+			MatrixReply[] replies = new MatrixReply[numOfServersRequired];
+
+			for(int i=0; i < replies.length; i++) {
+				replies[i] = stubs[i].parallelMatrixMultiplyBlock(requests[i].build());
+			}
+
+			for(int i=0; i < replies.length; i++) {
+				for(int j=0; j < replies[i].getCList().size(); j++) {
+					for (int k=0; k < replies[i].getCList().get(j).getNumberList().size(); k++) {
+						resp += replies[i].getCList().get(j).getNumber(k) + " ";
+					}
+					resp += "<br>";
+				}
+			}
 		}
+		else {
+			int randomNumber = (int)(Math.random()*8);
+			MatrixRequest.Builder request = MatrixRequest.newBuilder();
+
+			for (int row = 0; row < m1.length; row++) {
+				Row.Builder tempRow = Row.newBuilder();
+				for (int col = 0; col < m1[row].length; col++) {
+					tempRow.addNumber(m1[row][col]);
+				}
+				request.addA(tempRow);
+			}
+
+			for (int row = 0; row < m2.length; row++) {
+				Row.Builder tempRow = Row.newBuilder();
+				for (int col = 0; col < m2[row].length; col++) {
+					tempRow.addNumber(m2[row][col]);
+				}
+				request.addB(tempRow);
+			}
+
+
+
+
+			MatrixReply C=stubs[randomNumber].multiplyBlock(request.build());
+			List<Row> arrayListC = C.getCList();
+
+			for (int row = 0; row < arrayListC.size(); row++) {
+				for (int col = 0; col < arrayListC.get(row).getNumberList().size(); col++) {
+					resp += arrayListC.get(row).getNumber(col) + " ";
+				}
+				resp += "<br>";
+			}
+		}
+		
 
 		return resp;
-}
+	}
 }
