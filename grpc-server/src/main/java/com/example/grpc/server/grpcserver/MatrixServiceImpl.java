233c7e26e08606a1427ec7a9cf750e4248c4154a
Azim Islam <ec19205@qmul.ac.uk>
2022-04-06 16:29:58 +0100

    Debugging

--- grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
+++ grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
@@ -89,7 +89,7 @@ public class MatrixServiceImpl extends MatrixServiceGrpc.MatrixServiceImplBase
 				System.out.println("Test 2");
 				for (int i = 0; i < A.get(row).getNumberList().size(); i++) {
 					System.out.println("Test 3");
-					System.out.println(A.get(row).getNumber(i));
+					System.out.println(B.get(row).getNumberList().size());
 					System.out.println(i);
 					Double firstNum = A.get(row).getNumber(i);
 					Double secondNum = B.get(i).getNumber(col);
