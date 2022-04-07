ae312a12a7155638fc3ca1487b656ba5ec66848f
Azim Islam <ec19205@qmul.ac.uk>
2022-04-07 15:53:01 +0100

    Debug

--- grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
+++ grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
@@ -89,7 +89,7 @@ public class MatrixServiceImpl extends MatrixServiceGrpc.MatrixServiceImplBase
 			}
 		}
 
-		System.out.println(B.get(1).getNumber(0));
+		System.out.println(B.get(1));
 
 		for (int row = request.getRangeList().get(0); row < request.getRangeList().get(1); row++) {
 			for (int col = 0; col < A.get(row).getNumberList().size(); col++) {
